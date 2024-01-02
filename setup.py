47f31ad2836521f52200ca7d00c8f9f87babae4a
CielAl <cielmercy@gmail.com>
2024-01-01 20:18:25 -0500

    prepare setup.py

--- setup.py
+++ setup.py
@@ -8,14 +8,21 @@ with open("./torch_staintools/version.py") as fp:
     exec(fp.read(), version_dict)
 version = version_dict["__version__"]
 
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='torch-staintools',
     version=version,
     packages=find_packages(),
-    url='',
+    url='https://github.com/CielAl/torch-staintools',
     license='MIT',
     author='Y Z',
     author_email='cielmercy@gmail.com',
-    description='GPU-accelerated stain normalization as nn.Module.',
+    description='GPU-accelerated stain normalization and augmentation',
     install_requires=requirements,
+    python_requires='>=3.10',
+
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 )
